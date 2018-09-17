a6b4380929f9c660206192d4735b77877b597a0a
Redspeed93 <31858506+Redspeed93@users.noreply.github.com>
2018-09-17 12:49:25 +0200

    V1

---
+++ RandomPersonsMathLibrary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMathDLL;
+
+namespace RandomPersonsMathLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMathDLL.MyMathClass math = new MyMathClass();
+            Console.WriteLine(math.Add(3, 4));
+            Console.WriteLine(math.Divide(4, 2));
+            Console.WriteLine(math.Multiply(5, 88));
+            Console.WriteLine(math.Subtract(5, 6));
+            Console.ReadLine();
+        }
+    }
+}
